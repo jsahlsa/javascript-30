bd147aa3f8bbf5e9f40a9f53de86185b8f1dd9df
jsahlsa <joe.sahlstrom@gmail.com>
2021-03-10 23:46:40 +0000

    finished drum kit

--- 01-drum-kit/main.js
+++ 01-drum-kit/main.js
@@ -1,7 +1,22 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    console.log(audio);
+    const key = this.document.querySelector(`.key[data-key='${e.keyCode}']`)
+    //console.log(audio);
     if (!audio) return; //stop function from running
-    audio.load();
+    audio.currentTime = 0;
     audio.play();
-});
+    this.console.log(key);
+    key.classList.add('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
