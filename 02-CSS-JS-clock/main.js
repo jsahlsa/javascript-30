8daf4ad8d974dd6f05def7cdac9af2252fb52434
jsahlsa <joe.sahlstrom@gmail.com>
2021-03-19 22:38:47 +0000

    fixed wonky clock behavior

--- 02-CSS-JS-clock/main.js
+++ 02-CSS-JS-clock/main.js
@@ -2,19 +2,32 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+const now = new Date();
+const seconds = now.getSeconds();
+const mins = now.getMinutes();
+const hours = now.getHours();
+
+let secondsDegrees = ((seconds / 60) * 360) + 90;
+let minDegrees = ((mins / 60) * 360) + 90;
+let hourDegrees = ((hours / 12) * 360) + 90;
+
+
+
+
+
+
+
+
 function setDate() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsDegrees += 6;
+    minDegrees += 0.1;
+    hourDegrees += 0.0083
+
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = now.getMinutes();
     console.log(mins);
-    const minDegrees = ((mins / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minDegrees}deg)`;
 
-    const hours = now.getHours();
-    const hourDegrees = ((hours / 12) * 360) + 90;
     console.log(hours);
     hourHand.style.cssText = `transform: rotate(${hourDegrees}deg);`;
 
