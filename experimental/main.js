1d7e840539a62b9054180915b3c29b90b9fe9bfb
jsahlsa <joe.sahlstrom@gmail.com>
2021-03-29 22:34:58 +0000

    completed exercises for 1st chp of eloquent javascript & finished slide in scroll

--- experimental/main.js
+++ experimental/main.js
@@ -1,3 +1,10 @@
+
+
+const btnTwo = document.querySelector('.btnTwo');
+const btn = document.querySelector('.clap');
+const unicornBtn = document.querySelector('.unicorns');
+const hashPara = document.querySelector('.hash__para');
+
 function playSound() {
     const audio = document.querySelector('audio');
     audio.currentTime = 0;
@@ -5,10 +12,56 @@ function playSound() {
     console.log(audio);
 }
 
-const btn = document.querySelector('button');
-const unicornBtn = document.querySelector('.unicorns');
+function hashRow() {
+    let hash = '#';
+    //console.log(e);
+    for (let i = 0; i < 7; i++) {
+        console.log(hash);
+        hashPara.innerHTML += hash + '\n';
+        hash = hash + '#';
 
+    }
+    //console.log(allHashes);
+}
+
+btnTwo.addEventListener('click', hashRow);
 btn.addEventListener('click', playSound);
 unicornBtn.addEventListener('click', () => {
     cornify_add();
 });
+
+let fizz = 'fizz';
+let buzz = 'buzz';
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0 && i !== 0) {
+        console.log(fizz + buzz);
+        continue;
+    } else if (i % 3 === 0 && i !== 0) {
+        console.log(fizz);
+    } else if (i % 5 === 0 && i !== 0) {
+        console.log(buzz);
+    } else {
+        console.log(i);
+    }
+}
+
+let count = 0;
+let black = '#';
+let white = ' ';
+let num = 20;
+let lineOne = white + black;
+let lineTwo = black + white;
+console.log(lineOne.repeat(num / 2));
+
+for (let i = 0; i < num / 2; i++) {
+
+    console.log(lineOne.repeat(num / 2) + '\n');
+    console.log(lineTwo.repeat(num / 2) + '\n');
+
+}
+
+
+
+
+
