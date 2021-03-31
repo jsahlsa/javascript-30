730f31d7cb6549633d78396e5e80f763088b3a07
jsahlsa <joe.sahlstrom@gmail.com>
2021-03-31 22:24:59 +0000

    added delete button to local storage project

--- 15-local-storage-and-event-delegation/index.js
+++ 15-local-storage-and-event-delegation/index.js
@@ -1,6 +1,7 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 
+
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
 const addItem = function (e) {
@@ -23,13 +24,17 @@ const populateList = function (plates = [], platesList) {
             <li>
                 <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
                 <label for="item${i}">${plate.text}</label>
+                <input type="button" id="delete-item${i}" data-index=${i} />
+                <label for="delete-item${i}"></label>
             </li> 
         `;
     }).join('');
 }
 
 const toggleDone = function (e) {
-    if (!e.target.matches('input')) return;
+
+    if (!e.target.matches('input[type=checkbox]')) return;
+    console.log(e.target);
     const el = e.target;
     const index = el.dataset.index;
     items[index].done = !items[index].done;
@@ -37,7 +42,21 @@ const toggleDone = function (e) {
     populateList(items, itemsList);
 }
 
+const deleteItem = function (e) {
+    if (!e.target.matches('input[type=button]')) return;
+    console.log(e.target);
+    const el = e.target;
+    const index = el.dataset.index;
+    items.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+
+
 addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
+itemsList.addEventListener('click', deleteItem);
+
 
 populateList(items, itemsList);
