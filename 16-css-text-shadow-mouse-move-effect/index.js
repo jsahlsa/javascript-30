a3228aa806dab1df4b0039293c6487a99016f3af
jsahlsa <joe.sahlstrom@gmail.com>
2021-04-01 22:50:03 +0000

    finished mouse shadow

--- 16-css-text-shadow-mouse-move-effect/index.js
+++ 16-css-text-shadow-mouse-move-effect/index.js
@@ -1,6 +1,9 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
+const walk = 30;
+
+
 const shadow = function (e) {
     //console.log(e);
 
@@ -9,7 +12,16 @@ const shadow = function (e) {
     // const height = hero.offsetHeight; 
     const { offsetWidth: width, offsetHeight: height } = hero;
     let { offsetX: x, offsetY: y } = e;
-    console.log(x, y);
+
+    if (this !== e.target) {
+        x += e.target.offsetLeft;
+        y += e.target.offsetTop;
+    };
+
+    const xWalk = (x / width * walk) - (walk / 2);
+    const yWalk = (y / height * walk) - (walk / 2);
+    text.style.textShadow = `${xWalk}px ${yWalk}px 10px rgba(0,0,0,0.17)`;
+    console.log(xWalk, yWalk);
 }
 
 hero.addEventListener('mousemove', shadow);
